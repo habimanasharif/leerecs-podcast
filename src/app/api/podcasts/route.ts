5de14a84f1b8dcd8d9cdcdc05ef85586f3f628a8
habimanasharif <hdidiersharif@gmail.com>
2024-12-14 15:00:05 +0200

    get some

--- src/app/api/podcasts/route.ts
+++ src/app/api/podcasts/route.ts
@@ -13,28 +13,29 @@ interface Feed {
 const fetchRSSFeed = async (url: string): Promise<Feed> => {
   try {
     const cachedFeed = cache.get<any>(url);
-    // if (cachedFeed) {
-    //   return cachedFeed.items.map((item:any)=>{
-    //     const title = item.title.split(" with ")
-    //     const artist =title[1] ? title[1]: "leerecs"
-    //     return {
-    //      imageUrl: item.itunes.image,
-    //      musicUrl: item.enclosure?.url,
-    //      artist: artist.split(" (")[0],
-    //      musicName: item.title,
-    //      description: item.contentSnippet,
-    //          }
-    //    });
-    // }
+    if (cachedFeed) {
+      return cachedFeed.items.map((item:any)=>{
+        const title = item.title.split(" with ")
+        const artist =title[1] ? title[1]: "leerecs"
+        return {
+         imageUrl: item.itunes.image,
+         musicUrl: item.enclosure?.url,
+         artist: artist.split(" (")[0],
+         musicName: item.title,
+         description: item.contentSnippet,
+             }
+       });
+    }
 
     const feed: any = await parser.parseURL(url);
     cache.set(url, feed);
     return feed.items.map((item:any,index:number)=>{
-      
+      const title = item.title.split(" with ")
+        const artist =title[1] ? title[1]: "leerecs"
      return {
       imageUrl: item.itunes.image,
       musicUrl: item.enclosure?.url,
-      artist: "leerecs",
+      artist: artist,
       musicName: item.title,
       description: item.contentSnippet,
           }
